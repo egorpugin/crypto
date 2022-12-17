860fb93504e751a35e1029129a978e58809f6a93
Egor Pugin <egor.pugin@gmail.com>
2022-12-17 03:23:55 +0300

    Initial commit.

---
+++ sw.cpp
@@ -0,0 +1,15 @@
+void build(Solution &s) {
+    // Uncomment to make a project. Also replace s.addTarget(). with p.addTarget() below.
+    // auto &p = s.addProject("myproject");
+    // p += Git("enter your url here", "enter tag here", "or branch here");
+
+    auto &t = s.addTarget<Executable>("mycrypto");
+    t += cpp23;
+    t += "src/main.cpp";
+
+
+    // add deps here
+    // example:
+    // t += "org.sw.demo.someproject"_dep;
+    
+}
