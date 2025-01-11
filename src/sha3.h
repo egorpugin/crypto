bf1f22c57da997e7eef2e29625db2935f71fb455
Egor Pugin <egor.pugin@gmail.com>
2025-01-11 17:05:20 +0300

    [sha3] Remove pad condition as the code is equal.

--- src/sha3.h
+++ src/sha3.h
@@ -115,14 +115,9 @@ struct keccak : keccak_p<1600> {
         auto q = (r - (bitlen % r)) / 8;
         uint8_t q21 = Padding | (1 << (log2floor(Padding) + 1));
         uint8_t q22 = 0x80;
-        if (q == 1) {
-            d[blockpos++] ^= q21 | q22;
-        } else {
-            d[blockpos++] ^= q21;
-            //memset(d + blockpos, 0, q - 2);
-            blockpos += q - 2;
-            d[blockpos++] ^= q22;
-        }
+        d[blockpos++] ^= q21;
+        blockpos += q - 2;
+        d[blockpos++] ^= q22;
         permute();
     }
     auto digest() noexcept {
