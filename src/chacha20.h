5a6a3b845207e08bf7b69c4af1333e7bc229b95a
Egor Pugin <egor.pugin@gmail.com>
2025-01-14 14:48:57 +0300

    Improve chacha.

--- src/chacha20.h
+++ src/chacha20.h
@@ -10,16 +10,16 @@ struct salsa_chacha_index {
 };
 
 template <auto idx>
-void salsa_chacha_block(auto &&qr, uint32_t *in, uint32_t *out, int num_rounds) {
+void salsa_chacha_block(auto &&f, uint32_t *in, uint32_t *out, int num_rounds) {
 	uint32_t x[16];
     memcpy(x, in, sizeof(x));
+    auto round = [&](auto &&v) {
+		f(x, v[0], v[1], v[2], v[3]);
+		f(x, v[4], v[5], v[6], v[7]);
+		f(x, v[8], v[9], v[10], v[11]);
+		f(x, v[12], v[13], v[14], v[15]);
+    };
 	for (int i = 0; i < num_rounds; i += 2) {
-        auto round = [&](auto &&v) {
-		    qr(x, v[0], v[1], v[2], v[3]);
-		    qr(x, v[4], v[5], v[6], v[7]);
-		    qr(x, v[8], v[9], v[10], v[11]);
-		    qr(x, v[12], v[13], v[14], v[15]);
-        };
         round(idx.round1);
         round(idx.round2);
 	}
@@ -39,7 +39,7 @@ void salsa_block(uint32_t *in, uint32_t *out, int num_rounds = 20) {
         10, 11,  8,  9,
         15, 12, 13, 14,
     }};
-    auto f = [&](uint32_t *x, int a, int b, int c, int d) {
+    constexpr auto f = [](uint32_t *x, int a, int b, int c, int d) {
         x[b] ^= std::rotl(x[a] + x[d], 7);
         x[c] ^= std::rotl(x[b] + x[a], 9);
         x[d] ^= std::rotl(x[c] + x[b], 13);
@@ -59,7 +59,7 @@ void chacha_block(uint32_t *in, uint32_t *out, int num_rounds) {
         2, 7, 8, 13,
         3, 4, 9, 14
     }};
-    auto f = [&](uint32_t *x, int a, int b, int c, int d) {
+    constexpr auto f = [](uint32_t *x, int a, int b, int c, int d) {
         x[a] += x[b];
         x[d] = std::rotl(x[d] ^ x[a], 16);
         x[c] += x[d];
@@ -74,22 +74,27 @@ void chacha_block(uint32_t *in, uint32_t *out, int num_rounds) {
 
 template <auto N>
 struct chacha {
-    uint32_t s[16]{
+    uint32_t s[4]{
         0x61707865,
         0x3320646e,
         0x79622d32,
         0x6b206574,
     };
+    uint32_t key[8];
+    uint32_t counter;
+    uint32_t nonce[3];
     uint8_t block[64];
 
     chacha(uint8_t *key, uint8_t *nonce, uint32_t counter = 0) {
-        chacha_init_state(key, counter, nonce);
+        memcpy(this->key, key, sizeof(this->key));
+        set_counter(counter);
+        memcpy(this->nonce, nonce, sizeof(this->nonce));
         chacha_block(s, (uint32_t*)block, N);
     }
     void cipher(uint8_t *in, uint8_t *out, uint32_t inlen) {
         for (int i = 0; i < inlen; i += 64) {
             chacha_block(s, (uint32_t*)block, N);
-            ++s[12];
+            ++counter;
             for (int j = i; j < i + 64; j++) {
                 if (j >= inlen) {
                     break;
@@ -99,17 +104,7 @@ struct chacha {
         }
     }
     void set_counter(uint32_t counter) {
-        s[12] = counter;
-    }
-private:
-    void chacha_init_state(uint8_t *key, uint32_t counter, uint8_t nonce[12]) {
-        for (int i = 0; i < 8; i++) {
-            s[4 + i] = *(uint32_t*)(key + i * 4);
-        }
-        s[12] = counter;
-        for (int i = 0; i < 3; i++) {
-            s[13 + i] = *(uint32_t*)(nonce + i * 4);
-        }
+        this->counter = counter;
     }
 };
 using chacha20 = chacha<20>;
