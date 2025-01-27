8b28c75fd7a4f84bf2e379d3bf3a078987c1438b
Egor Pugin <egor.pugin@gmail.com>
2025-01-27 20:07:20 +0300

    Implement random for *nix.

--- src/random.h
+++ src/random.h
@@ -1,25 +1,38 @@
-#include "helpers.h"
-
 #pragma once
 
+#include "helpers.h"
+
+#ifdef _WIN32
 #define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
 #include <wincrypt.h>
+#else
+#include <unistd.h>
+#endif
 
 namespace crypto {
 
+auto get_random_secure_bytes(uint8_t *p, size_t len) {
+#ifdef _WIN32
+    BCryptGenRandom(0, p, len, BCRYPT_USE_SYSTEM_PREFERRED_RNG);
+#else
+    if (getentropy(p, len) == -1) {
+        throw std::runtime_error{"cannot get entropy"};
+    }
+#endif
+}
 auto get_random_secure_bytes(unsigned n) {
     std::vector<uint8_t> v;
     v.resize(n);
-    BCryptGenRandom(0, v.data(), n, BCRYPT_USE_SYSTEM_PREFERRED_RNG);
+    get_random_secure_bytes(v.data(), n);
     return v;
 }
 template <auto N>
 void get_random_secure_bytes(uint8_t (&v)[N]) {
-    BCryptGenRandom(0, v, N, BCRYPT_USE_SYSTEM_PREFERRED_RNG);
+    get_random_secure_bytes(v, N);
 }
 void get_random_secure_bytes(auto &v) {
-    BCryptGenRandom(0, (uint8_t*)v.data(), v.size(), BCRYPT_USE_SYSTEM_PREFERRED_RNG);
+    get_random_secure_bytes((uint8_t*)v.data(), v.size());
 }
 
 } // namespace crypto
