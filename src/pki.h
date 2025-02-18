c6f8554caf3d640bed4bf8f7005fcdd293f2088d
Egor Pugin <egor.pugin@gmail.com>
2025-02-18 21:11:39 +0300

    Update.

--- src/pki.h
+++ src/pki.h
@@ -54,6 +54,13 @@ struct public_key_infrastructure {
 
         return asn1_sequence::make(str);
     }
+    static auto make_pem(auto &&name, auto &&data) {
+        auto b64 = base64::encode(data);
+        for (int i = 64; i < b64.size(); i += 64) {
+            b64.insert(b64.begin() + i++, '\n');
+        }
+        return std::format("-----BEGIN {}-----\n{}\n-----END {}-----\n", name, b64, name);
+    }
     auto make_ca(auto &&name) {
         auto version = asn1_sequence::make(asn1_integer::make(3 - 1)); // ver 3
         version[0] = 0xA0;
@@ -88,6 +95,7 @@ struct public_key_infrastructure {
                 asn1_octet_string::make(asn1_octet_string::make(sha1::digest(public_key_bits)))
             )
         );
+        // asn1_x509_extensions
         auto x509_exts = asn1_sequence::make(exts);
         x509_exts[0] = 0xA3;
 
@@ -105,15 +113,44 @@ struct public_key_infrastructure {
         std::vector<u8> h2{std::from_range, h | std::views::reverse};
         auto rsig = c.sign(h2);
 
-        auto alg = asn1_sequence::make(asn1_oid::make(oid::gost2012Signature256));
         auto sig = asn1_bit_string::make(rsig);
 
-        auto cert = asn1_sequence::make(tbs_certificate, alg, sig);
-        auto certb = std::format("-----BEGIN CERTIFICATE-----\n{}\n-----END CERTIFICATE-----\n", base64::encode(cert));
+        auto cert = asn1_sequence::make(tbs_certificate, signature, sig);
+
+
         auto fn = root / name;
-        mmap_file<> m{path{fn} += ".crt"};
-        m.alloc_raw(certb.size());
-        memcpy(m.data(), certb.data(), certb.size());
+        {
+            mmap_file<> m{path{fn} += ".crt"};
+            auto t = make_pem("CERTIFICATE"sv, cert);
+            m.alloc_raw(t.size());
+            memcpy(m.data(), t.data(), t.size());
+        }
+        // pk
+        {
+            auto oid = asn1_sequence::make(
+                asn1_oid::make(oid::gost_r34102001_param_set_a)
+            );
+            oid[0] = 0xA0;
+            auto pubk = asn1_sequence::make(
+                //public_key_bits,
+                //asn1_bit_string::make(public_key_bits),
+                asn1_bit_string::make(bytes_concept{&pub,sizeof(pub)})
+            );
+            pubk[0] = 0xA1;
+            auto pk = asn1_sequence::make(
+                asn1_integer::make(1),
+                asn1_octet_string::make(c.private_key_),
+                oid,
+                pubk
+            );
+
+            mmap_file<> m{path{fn} += ".key"};
+            auto t = make_pem("EC PRIVATE KEY"sv, pk);
+            m.alloc_raw(t.size());
+            memcpy(m.data(), t.data(), t.size());
+        }
+
+
     }
 };
 
