e2a60196f201d4ccc150375fdfec5a2c4b3659d1
Egor Pugin <egor.pugin@gmail.com>
2025-04-01 02:24:19 +0300

    Fix constant.

--- src/helpers.h
+++ src/helpers.h
@@ -127,7 +127,7 @@ struct bytes_concept {
     auto empty() const { return size() == 0; }
     auto subspan(size_t start, size_t sz = -1) const {
         auto b = p + start;
-        if (sz == SIZE_T_MAX || sz >= this->sz) {
+        if (sz == SIZE_MAX || sz >= this->sz) {
             return bytes_concept{b, this->sz - start};
         }
         sz = std::min<size_t>(this->sz - start, sz);
