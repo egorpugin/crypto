1c1bd337e460ceafe288c3c6d02a02a66380d53a
Egor Pugin <egor.pugin@gmail.com>
2026-01-06 23:28:02 +0300

    [mlkem] Remove unused functions.

--- src/mlkem.h
+++ src/mlkem.h
@@ -3,10 +3,9 @@
 
 #pragma once
 
-#include "random.h"
 #include "sha3.h"
 
-// check mlkem in boring ssl
+// check mlkem struct (pkey,skey) members in boring ssl
 // https://boringssl.googlesource.com/boringssl.git/+/chromium-stable/crypto/mlkem/mlkem.cc
 
 // TODO: SLH-DSA?
@@ -369,53 +368,6 @@ consteval bool check_l(const size_t l) {
 // Compile-time check to ensure that operand matrices are having compatible dimension for matrix multiplication.
 consteval bool check_matrix_dim(const size_t a_cols, const size_t b_rows) {
     return !static_cast<bool>(a_cols ^ b_rows);
-}
-
-// Compile-time check to ensure that both K-PKE, ML-KEM key generation routine is invoked with proper parameter set.
-//
-// See table 2 of ML-KEM specification https://doi.org/10.6028/NIST.FIPS.203.
-consteval bool check_keygen_params(const size_t k, const size_t eta1) {
-    bool flg0 = (k == 2) && (eta1 == 3);
-    bool flg1 = (k == 3) && (eta1 == 2);
-    bool flg2 = (k == 4) && (eta1 == 2);
-
-    return flg0 || flg1 || flg2;
-}
-
-// Compile-time check to ensure that K-PKE encryption routine is invoked with proper parameter set.
-//
-// See table 2 of ML-KEM specification https://doi.org/10.6028/NIST.FIPS.203.
-consteval bool check_encrypt_params(const size_t k, const size_t η1, const size_t η2, const size_t du, const size_t dv) {
-    bool flg0 = (k == 2) && (η1 == 3) && (η2 == 2) && (du == 10) && (dv == 4);
-    bool flg1 = (k == 3) && (η1 == 2) && (η2 == 2) && (du == 10) && (dv == 4);
-    bool flg2 = (k == 4) && (η1 == 2) && (η2 == 2) && (du == 11) && (dv == 5);
-
-    return flg0 || flg1 || flg2;
-}
-
-// Compile-time check to ensure that K-PKE decryption routine is invoked with proper parameter set.
-//
-// See table 2 of ML-KEM specification https://doi.org/10.6028/NIST.FIPS.203.
-consteval bool check_decrypt_params(const size_t k, const size_t du, const size_t dv) {
-    bool flg0 = (k == 2) && (du == 10) && (dv == 4);
-    bool flg1 = (k == 3) && (du == 10) && (dv == 4);
-    bool flg2 = (k == 4) && (du == 11) && (dv == 5);
-
-    return flg0 || flg1 || flg2;
-}
-
-// Compile-time check to ensure that ML-KEM encapsulation routine is invoked with proper parameter set.
-//
-// See table 2 of ML-KEM specification https://doi.org/10.6028/NIST.FIPS.203.
-consteval bool check_encap_params(const size_t k, const size_t η1, const size_t η2, const size_t du, const size_t dv) {
-    return check_encrypt_params(k, η1, η2, du, dv);
-}
-
-// Compile-time check to ensure that ML-KEM encapsulation routine is invoked with proper parameter set.
-//
-// See table 2 of ML-KEM specification https://doi.org/10.6028/NIST.FIPS.203.
-consteval bool check_decap_params(const size_t k, const size_t η1, const size_t η2, const size_t du, const size_t dv) {
-    return check_encap_params(k, η1, η2, du, dv);
 }
 
 } // namespace ml_kem_params
